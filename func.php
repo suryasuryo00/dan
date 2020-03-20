493ae7f11cc8c48c2b0054d4879354d048951fe4
suryasuryo00 <34580117+suryasuryo00@users.noreply.github.com>
2020-03-20 07:38:04 +0700

    Update func.php

--- func.php
+++ func.php
@@ -12,7 +12,7 @@ $header[] = "X-AppVersion: 3.48.2"; // ubah sesuai clone lu
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: en_ID";
-$header[] = "X-Location: -6.224058,106.877913";
+$header[] = "X-Location: -7,9667900, 112,6248760";
 $header[] = "X-Location-Accuracy: 0.0";
 if ($pin):
 $header[] = "pin: $pin";
